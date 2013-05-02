d27a520fee1442d6d4e2f7f3af8369fe81a102f7
ilSavoOffice <marco.savoia@wininjob.com>
2013-05-02 16:54:18 +0200

    Aggiunto Feature n.2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 primo
 =====
 my first new line
+2013-05-02 16:53 corretto di nuovo il readme dentro a Feature2
---
+++ newFeature2/file1.php
@@ -0,0 +1,17 @@
+<?php  
+/**
+* 
+*/
+class ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+
+	public function FunctionName($value='')
+	{
+		# code...
+	}
+}
