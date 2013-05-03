294dfacf17710a45d473b47df9df357720f3e332
ilSavo <marco.savoia@gmail.com>
2013-05-03 11:22:44 +0200

    Added a new feature

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
 	</head>
 	<body>
 		<?php echo 'ciao!!' ?>
-		<a href="page1.php">page1</a>
+		<a href="page1.php">page1</a> <a href="page2.php">page2</a> 
 	</body>
 </html>
---
+++ page2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Page2</title>
+	</head>
+	<body>
+		This is the second page
+		<a href="index.php">index</a>
+	</body>
+</html>
